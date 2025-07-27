e7e934d676d2a231ac366afa1e0486920ba0e899
hulkbusterks <105232516+hulkbusterks@users.noreply.github.com>
2025-07-27 16:26:16 +0000

    removed the unnecessary comment lines and added a meaningful commit message

--- config/client.py
+++ config/client.py
@@ -7,15 +7,9 @@ load_dotenv()
 profile_arn = os.getenv("BEDROCK_INFERENCE_PROFILE_ARN")
 region = os.getenv("AWS_DEFAULT_REGION")
 
-from google import genai
-
-client = genai.Client(
-        api_key= os.getenv('GOOGLE_API_KEY'),
+model = ChatBedrockConverse(
+    model=profile_arn,
+    provider="anthropic",
+    temperature=0.5,
+    region_name=region
 )
-model = "gemini-2.5-flash-lite"
-# model = ChatBedrockConverse(
-#     model=profile_arn,
-#     provider="anthropic",
-#     temperature=0.5,
-#     region_name=region
-# )
