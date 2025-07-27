7dbbba7c079d45adf2cae5d932f8a61985d8b50d
hulkbusterks <105232516+hulkbusterks@users.noreply.github.com>
2025-07-27 16:24:11 +0000

    HOTFIX: removed circular refrences

--- config/client.py
+++ config/client.py
@@ -7,10 +7,15 @@ load_dotenv()
 profile_arn = os.getenv("BEDROCK_INFERENCE_PROFILE_ARN")
 region = os.getenv("AWS_DEFAULT_REGION")
 
+from google import genai
 
-model = ChatBedrockConverse(
-    model=profile_arn,
-    provider="anthropic",
-    temperature=0.5,
-    region_name=region
+client = genai.Client(
+        api_key= os.getenv('GOOGLE_API_KEY'),
 )
+model = "gemini-2.5-flash-lite"
+# model = ChatBedrockConverse(
+#     model=profile_arn,
+#     provider="anthropic",
+#     temperature=0.5,
+#     region_name=region
+# )
